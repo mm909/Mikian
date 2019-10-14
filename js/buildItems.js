a5c9398aaf8a763defd99f736679ef5d6d606c41
Mikian Musser <Bobar312@gmail.com>
2019-10-14 09:49:04 -0700

    Dynamic

---
+++ js/buildItems.js
@@ -0,0 +1,48 @@
+jQuery(document).ready(function() {
+  let categoryList = []
+  for (var i = 0; i < items.length; i++) {
+    if (!categoryList.includes(items[i].category)) {
+      categoryList.push(items[i].category)
+    }
+  }
+  console.log(categoryList);
+  for (var i = 0; i < items.length; i++) {
+    let objectives = "";
+
+    for (var j = 0; j < items[i].objectives.length; j++) {
+      objectives += "<li class='objective'>" + items[i].objectives[j] + "</li>"
+    }
+
+    if (items[i].objectives.length > 0) {
+      objectives = "<ul class='item-objective-list'> " + objectives + "</ul>"
+    }
+
+    let item = `
+    <div class="item">
+      <div class="profile"></div>
+      <div class="item-text">
+        <div class="item-header">
+          <div class="title-text">
+            <h3 class="workplace">
+              ` + items[i].workplace + `
+            </h3>
+            <p class="role">
+              <i>` + items[i].role + `</i>
+            </p>
+          </div>
+          <div class="item-date">
+            <p>
+              <i>` + items[i].date + `</i>
+            </p>
+          </div>
+        </div>
+        <div class="item-objectives">
+            ` + objectives + `
+        </div>
+      </div>
+    </div>
+    `
+    $jitem = $(item)
+    // $(".resume").append($jitem);
+  }
+});
---
+++ js/items.js
@@ -0,0 +1,143 @@
+items = [{
+    category: "Education",
+    workplace: "Computer Science, BS",
+    role: "<a href='https://www.unlv.edu/'>University of Nevada, Las Vegas</a>",
+    date: "Aug 2016 - Present",
+    objectives: ["Computer science senior | Expected graduation - May 2020 | GPA: 3.97"]
+  },
+  {
+    category: "Education",
+    workplace: "High School Valedictorian",
+    role: "<a href='https://schools.ccsd.net/virginvalley'>Virgin Valley High School</a>",
+    date: "Aug 2012 - May 2016",
+    objectives: ["Graduation May 2016 | GPA: 4.80"]
+  },
+  {
+    category: "Experience",
+    workplace: "Developer — <a class='Intellimind' href='https://www.intellimind.com/en'>Intellimind</a>",
+    role: "Las Vegas - Software Developer",
+    date: "Oct 2019 - Present",
+    objectives: []
+  },
+  {
+    category: "Experience",
+    workplace: "Teaching — <a class='unlvASC' href='https://www.unlv.edu/asc/'>Academic Success Center</a>",
+    role: "<a href='https://www.unlv.edu/'>UNLV</a> - Computer Science Tutor",
+    date: "Aug 2019 - Oct 2019",
+    objectives: [
+      "Taught students of all levels and ages how to make websites, games, and robots.",
+      "Lead classes about OOP in P5.js and game development in Javascript and GameMaker.",
+      "Developed project-based web development curriculum for twenty-five to fifty advanced students."
+    ]
+  },
+  {
+    category: "Experience",
+    workplace: "Teaching — <a class='codecentral' href='https://mycodecentral.com/'>Code Central</a>",
+    role: "Las Vegas - Computer Science Mentor",
+    date: "Aug 2018 - Aug 2019",
+    objectives: [
+      "Tutored twenty-four core computer science, computer engineering, statistics, and math classes.",
+      "Measurably improved students' performance in class, on assignments, and during exams.",
+      "Effectively used teaching strategies to tutor multiple students at the same time."
+    ]
+  },
+  {
+    category: "Experience",
+    workplace: "Teaching — <a class='idtech' href='https://www.idtech.com/'>iD Tech</a>",
+    role: "<a href='https://www.rice.edu/'>Rice University</a> - Computer Science Instructor",
+    date: "May 2018 - Aug 2018",
+    objectives: [
+      "Lead a classroom of eight young kids to create video games and robots.",
+      "Created lesson plans and coding challenges for each day and presented material in fun and unique ways.",
+      "Balanced teaching the class and working with each student one on one to ensure each student was learning."
+    ]
+  },
+  {
+    category: "Experience",
+    workplace: "IT — <a class='unlvit' href='https://www.it.unlv.edu/'>UNLV OIT</a>",
+    role: "Field and Desktop Technician",
+    date: "Aug 2017 - May 2018",
+    objectives: [
+      "Worked with university faculty and staff to resolve computer, printer, and network issues.",
+      "Resolved common hardware issues, ordered and replaced hardware when needed.",
+      "Managed a service ticket queue of ten or more field requests."
+    ]
+  },
+  {
+    category: "Experience",
+    workplace: "IT — <a class='unlvit' href='https://www.it.unlv.edu/'>UNLV OIT</a>",
+    role: "Help Desk Technician",
+    date: "Aug 2016 - Aug 2017",
+    objectives: [
+      "Worked with students at the university to resolve common hardware and software issues.",
+      "Managed a queue of more than two-hundred at a time, quickly resolving issues.",
+      "Troubleshooted issues over the phone, through email, and in person."
+    ]
+  },
+  {
+    category: "Experience",
+    workplace: "IT — <a class='mesquite' href='https://www.mesquitenv.gov/'>City of Mesquite</a>",
+    role: "Technician Intern",
+    date: "May 2016 - Aug 2016",
+    objectives: [
+      "Assisted IT Technicians with day to day operations.",
+      "Documented tickets, service requests, and research."
+    ]
+  },
+  {
+    category: "Notable Projects",
+    workplace: "<a href='http://MikianMusser.com'>MikianMusser.com</a>",
+    role: "Personal web site - html/css/js/pug/sass ",
+    date: "Aug 2018 - Present",
+    objectives: [
+      "Iteratively designed and programmed a personal website to showcase projects and contact information.",
+      "Used CSS and HTML preprocessors such as SASS and PUG to increase productivity.",
+      "Taught myself the web programming languages through self guided intuition."
+    ]
+  },
+  {
+    category: "Notable Projects",
+    workplace: "<a href='https://github.com/dook-robotics'>Dook Robotics</a>",
+    role: "Senior Design - Automatic Pooper Scooper",
+    date: "Jul 2019 - Present",
+    objectives: [
+      "Created, labeled, and cleaned a database of over 1000 images and 3000 objects used for object detection.",
+      "Wrote scripts to interface with many hardware devices: motors, load cell, relay, voltage sensor",
+      "Used Tensorflow to train several different models based on the environment the robot is in.",
+      "Wrote scripts to automate nearly every part of the data processing pipeline."
+    ]
+  },
+  {
+    category: "Notable Projects",
+    workplace: "<a href='https://github.com/mm909/Convolutional-Neural-Network-CIFAR10'>Convolutional Neural Network</a> — <a href='https://www.cs.toronto.edu/~kriz/cifar.html'>CIFAR-10</a>",
+    role: "CNN to recognize CIFAR-10 images",
+    date: "Spring 2019t",
+    objectives: [
+      "Calculated the partial derivatives for backpropagation in a multi-layer CNN and implemented them in C.",
+      "Achieved 70% accuracy on the CIFAR-10 dataset using this network written in C without any libraries.",
+      "Implemented backpropagation optimizers such as momentum gradient descent to converge quickly."
+    ]
+  },
+  {
+    category: "Notable Projects",
+    workplace: "<a href='https://mm909.github.io/NeuralNet-Tester/'>Neural Network White Box</a> — <a href='http://yann.lecun.com/exdb/mnist/'>MNIST</a>",
+    role: "Application to visualize ANN predictions",
+    date: "Spring 2019",
+    objectives: [
+      "Developed an application that allows users to draw images on the screen on, send that image through the network, and display the network's predictions.",
+      "Created a sandbox to understand how well the neural network was learning.",
+      "Wrote documentation showing what the hidden layer of the network is seeing."
+    ]
+  },
+  {
+    category: "Other Projects",
+    workplace: "<a href='https://victortaksheyev.github.io/portfolio/pages/segDisplay.html'>Seven Segment Neural Network </a> — <a href='http://yann.lecun.com/exdb/mnist/'>MNIST</a>",
+    role: "Neural network predictions displayed on seven segment display",
+    date: "May 2019",
+    objectives: [
+      "Trained a KNN to recognize MNIST digits and used a video feed to predict what number was held up.",
+      "Used my application <a href='https://mm909.github.io/NeuralNet-Tester/'>\"Neural Network White Box\"</a> to allow the user to draw a digit to the screen and have it be displayed on the seven segment display.",
+      "Created a seven segment display using servos and an arduino microcontroller."
+    ]
+  }
+]
