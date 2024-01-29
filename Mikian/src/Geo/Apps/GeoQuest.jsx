a86f8e7593feb70f5461f4b27f71cf7169058d7c
Mikian Musser <Bobar312@gmail.com>
2024-01-28 17:40:55 -0800

    yuh yeee

--- Mikian/src/Geo/Apps/GeoQuest.jsx
+++ Mikian/src/Geo/Apps/GeoQuest.jsx
@@ -6,6 +6,13 @@ import GeoEngine from '@/Geo/Engine/GeoEngine.jsx'
 function GeoQuest() {
     const { geoJsonData } = useContext(GeoContext)
 
+    const items = geoJsonData?.features.map((item, index) => item.properties.ADMIN) ?? [];
+    const chosen_item = items[Math.floor(Math.random() * items.length)];
+
+    // const onClick = () => {
+    //     d3.select(this).style('fill', 'Yellow');
+    // }
+
     return (
         <>  
             <GeoEngine />
--- Mikian/src/Geo/Engine/GeoEngine.jsx
+++ Mikian/src/Geo/Engine/GeoEngine.jsx
@@ -11,14 +11,19 @@ function GeoEngine() {
     const projection = d3.geoEquirectangular()
     const geoGenerator = d3.geoPath().projection(projection);
   
+    const zoom = d3.zoom().on('zoom', (e) => {
+        d3.select('svg g').attr('transform', e.transform);
+    });
+
     useEffect(() => {
-      if (geoJsonData) {
+        if (!geoJsonData) return;
         d3.select('#GeoEngine g.map')
-          .selectAll('path')
-          .data(geoJsonData.features)
-          .join('path')
-          .attr('d', geoGenerator)
-      }
+            .selectAll('path')
+            .data(geoJsonData.features)
+            .join('path')
+            .attr('d', geoGenerator)
+
+        d3.select('svg').call(zoom);
     }, [geoJsonData]);
 
     return (
@@ -26,7 +31,7 @@ function GeoEngine() {
             <div id="GeoEngine" className='w-full h-full'>
                 {
                     geoJsonData == null ? <EmptyMap /> :
-                    <svg viewBox={`0 0 500 500`} height="100%" width='100%'>
+                    <svg viewBox={`0 0 250 250`} height="100%" width='100%'>
                         <g className="map" transform=""></g>
                     </svg>
                 }
